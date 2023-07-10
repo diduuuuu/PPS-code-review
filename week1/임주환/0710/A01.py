986b7b32d4b45d97564575d303924700a2733c08
wnghks7787 <74361097+wnghks7787@users.noreply.github.com>
2023-07-11 00:44:32 +0900

    0710

---
+++ week1/임주환/0710/A01.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+
+        i = 0
+        j = 0
+        result = 0
+
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                i += 1
+                j += 1
+                result += 1
+            else:
+                j += 1
+
+        return result
