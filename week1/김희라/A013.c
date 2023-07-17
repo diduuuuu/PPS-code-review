dba78106232e8e9b785120ca0f7856f7f0052fa2
KimHeera <91535597+KimHeera@users.noreply.github.com>
2023-07-17 16:23:44 +0900

    Add files via upload

---
+++ week1/김희라/A013.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int singleNumber(int* nums, int numsSize) {
+    int result = 0;
+    
+    for (int i = 0; i < numsSize; i++) {
+        result ^= nums[i];
+    }
+    
+    return result;
+}
